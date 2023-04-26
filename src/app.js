388b54e970a3a64d3e5d14dadb6611a39e10377a
Magdalena Nolde <magdalena@nolde.de>
2023-04-26 21:12:08 +0200

    finish task

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
 import Form from "./components/Form";
 import GlobalStyle from "./components/Styled/Global";
 import ResetStyle from "./components/Styled/Reset";
+
+
 
 function App() {
   return (
@@ -11,9 +12,16 @@ function App() {
       <ResetStyle />
       <GlobalStyle />
       <Form />
+      
     </>
   );
 }
+// 
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
 
-const rootElement = document.getElementById("root");
-ReactDOM.createRoot(rootElement).render(<App />);
+)
+
+// ReactDOM.createRoot(rootElement).render(<App />);
+// 
